73b6c3c6a0f010bd9de6964da91b33f712e98f81
IzMichael <themichael77@outlook.com>
2020-10-03 09:42:35 +1300

    IzMichael/Fix-Docs-README (#9)
    Adds setPageC(path, page) to docsHandler.js

--- assets/js/docsHandler.js
+++ assets/js/docsHandler.js
@@ -18,3 +18,24 @@ function setPage(section, page) {
     var location = '<a href="/">Home</a> / <a href="/docs">Documentation</a> / ' + section + ' / ' + page
     breadcrumb.innerHTML = location;
 }
+
+function setPageC(path, page) {
+    // Declare Element Variables
+    var container = document.getElementById("docsContainer");
+    var breadcrumb = document.getElementById("breadcrumb");
+    var show = document.getElementById("docs");
+
+    // Adding .md to the end of 'page' and fixing the filepath
+    var pageMD = path + page + '.md'
+
+    // Changing the src attribute value
+    show.setAttribute("src", pageMD);
+
+    // Reloading the Page Container
+    var content = container.innerHTML
+    container.innerHTML = content;
+
+    // Set Breadcrumb
+    var location = '<a href="/">Home</a> / <a href="/docs">Documentation</a> ' + path + ' ' + page
+    breadcrumb.innerHTML = location;
+}
